15b76a027ec389b80a4aab499665e13c3e8ad4b3
vf <valentin.flageat@etu.enseeiht.fr>
2020-10-26 23:53:13 +0100

    Add launch screen

--- app/src/main/java/com/vf/photobank/ui/MainActivity.kt
+++ app/src/main/java/com/vf/photobank/ui/MainActivity.kt
@@ -49,6 +49,7 @@ class MainActivity : AppCompatActivity() {
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewPager = view_pager
